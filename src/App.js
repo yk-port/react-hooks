719f5d8124b01f153fdf2c6aeea99db6ad628928
yokokura <yokokura0413@gmail.com>
2020-04-07 11:05:23 +0900

    feat: オブジェクトの更新の処理

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-// import Counter from './components/Counter';
 import CounterHook from './components/CounterHook';
+import FormHook from './components/FormHook';
 
 function App() {
   return (
     <div className="App">
       <CounterHook />
+      <FormHook />
     </div>
   );
 }
---
+++ src/components/FormHook.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+function FormHook() {
+  const [name, setName] = useState({ firstName: '', lastName: '' });
+  return (
+    <div>
+      <div>
+        <p>FirstName: {name.firstName}</p>
+        <p>LastName: {name.lastName}</p>
+      </div>
+      <form>
+        <input
+          type="text"
+          value={name.firstName}
+          onChange={e => setName({ ...name, firstName: e.target.value })}
+        />
+        <input
+          type="text"
+          value={name.lastName}
+          onChange={e => setName({ ...name, lastName: e.target.value })}
+        />
+      </form>
+    </div>
+  )
+}
+
+export default FormHook
