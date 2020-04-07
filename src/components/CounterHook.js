6dcea84ede5ed2d54957999f4abfed8090fc9e10
yokokura <yokokura0413@gmail.com>
2020-04-07 10:49:23 +0900

    feat: prevを使ってカウントを更新

--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -4,14 +4,20 @@ function CounterHook() {
   const [count, setCount] = useState(0);
 
   const incrementCount = () => {
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
+  }
+
+  const incrementCountTen = () => {
+    for (let i = 0; i < 10; i++) {
+      setCount(prevCount => prevCount + 1);
+    }
   }
 
   return (
     <div>
       <h1>{count}</h1>
-      <button
-        onClick={incrementCount}>count up</button>
+      <button onClick={incrementCount}>1 count up</button>
+      <button onClick={incrementCountTen}>10 count up</button>
     </div>
   )
 }
