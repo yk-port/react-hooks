dd08945a2ff83fe3a5fee114b4d0ad59dc290fc9
yokokura <yokokura0413@gmail.com>
2020-04-07 11:15:00 +0900

    feat: 配列の更新の処理

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import CounterHook from './components/CounterHook';
 import FormHook from './components/FormHook';
+import ItemHook from './components/ItemHook';
 
 function App() {
   return (
     <div className="App">
       <CounterHook />
       <FormHook />
+      <ItemHook />
     </div>
   );
 }
